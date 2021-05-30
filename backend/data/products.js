0faa8cbf2da479dcd46993211001a94c588b0d72
AMAL2525 <amallaabidi25@gmail.com>
2021-05-30 12:28:19 +0100

    first commit

---
+++ backend/data/products.js
@@ -0,0 +1,76 @@
+const products = [
+    {
+      name: "Sunglass Brown",
+      imageUrl:
+        "https://images.unsplash.com/photo-1577803645773-f96470509666?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8c3VuZ2xhc3Nlc3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+      description:
+        "A brown sunglass for your cosy evenings.",
+      price: 100,
+      countInStock: 15,
+    },
+    {
+      name: "Sunglass Mauve",
+      imageUrl:
+        "https://images.unsplash.com/photo-1473496169904-658ba7c44d8a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80",
+      description:
+        "Can be used by any one at any time, for work or beach...",
+      price: 80,
+      countInStock: 10,
+    },
+    {
+      name: "Sunglass for man 1",
+      imageUrl:
+        "https://images.unsplash.com/photo-1567333126229-db29200c25f1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTA1fHxzdW5nbGFzc2VzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+      description:
+        "A vintage classic brown sunglass, a model that never dies.",
+      price: 110,
+      countInStock: 5,
+    },
+    {
+      name: "Sunglass the trend",
+      imageUrl:
+        "https://images.unsplash.com/photo-1581778571772-11f8c6af4103?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjE0fHxzdW5nbGFzc2VzfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+      description:
+        "It's the trend, be sur that you keep one in your purse .",
+      price: 70,
+      countInStock: 25,
+    },
+    {
+      name: "Sunglass for man 2",
+      imageUrl:
+        "https://images.unsplash.com/photo-1600024914363-d9bf85e438f0?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzUwfHxzdW5nbGFzc2VzfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+      description:
+        "A vintage classic brown sunglass, a model that never dies.",
+      price: 90,
+      countInStock: 4,
+    },
+    {
+      name: "Sunglass Cat eye",
+      imageUrl:
+        "https://images.unsplash.com/photo-1588713386671-6a34f044e7f8?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzY5fHxzdW5nbGFzc2VzfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+      description:
+        "A vintage classic brown sunglass, a model that never dies for women..",
+      price: 140,
+      countInStock: 10,
+    },
+    {
+      name: "Sunglass Classic",
+      imageUrl:
+        "https://images.unsplash.com/photo-1596769269298-dad4ec1070e3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzc1fHxzdW5nbGFzc2VzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+      description:
+        "For your important days.",
+      price: 200,
+      countInStock: 20,
+    },
+    {
+      name: "Sunglass Sport",
+      imageUrl:
+        "https://images.unsplash.com/photo-1606196480589-2af10fb10ab2?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDcwfHxzdW5nbGFzc2VzfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+      description:
+        "You can't jogg without them",
+      price: 150,
+      countInStock: 50,
+    },
+  ];
+  
+  module.exports = products;
